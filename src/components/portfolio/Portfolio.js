3e2cb15801c8bb5f060fe80e2432db5047c91432
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2022-11-23 09:55:22 +0100

    created portfolio page

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import "./portfolio.css"
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
-  )
+    <section id="portfolio">
+      <h1>Portfolio</h1>
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <img src="" alt="" className="portfolio-image" />
+          <h2>Project name</h2>
+          <a href="https://github.com" className="btn">GitHub</a>
+          <a href="site" className="btn btn-primary">Live Demo</a>
+        </article>
+      </div>
+    </section>
+    )
 }
 
 export default Portfolio
